832d07a5f65c0d75fc1cdf629c919fd95c88041b
Dieseled-UP <bourne.denis@gmail.com>
2017-07-26 22:04:12 +0100

    Adding code to scan and transmit using BLE

--- app/src/main/java/com/bournedev/masterscanner/BluetoothActivity.java
+++ app/src/main/java/com/bournedev/masterscanner/BluetoothActivity.java
@@ -4,36 +4,45 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Handler;
-import android.support.annotation.StringRes;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.charset.Charset;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class BluetoothActivity extends AppCompatActivity {
 
-    private final String TAG = MainActivity.class.getSimpleName();
+    private final String TAG = BluetoothActivity.class.getSimpleName();
 
     private BluetoothAdapter btAdapter;
     private BluetoothLeScanner bluetoothLeScanner;
+    private BluetoothLeAdvertiser advertiser;
+    private AdvertiseCallback advertisingCallback;
     private static final int REQUEST_ENABLE_BT = 1;
     private Button btnScan;
     private ListView listViewLE;
@@ -66,21 +75,27 @@ public class BluetoothActivity extends AppCompatActivity {
             finish();
         }
 
-        getBluetoothAdapterAndLeScanner();
-
         if (btAdapter != null && !btAdapter.isEnabled()) {
 
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
         }
 
+        btnScan = (Button) findViewById(R.id.start_blue);
+        Button btnStop = (Button) findViewById(R.id.stop_blue);
+
+        if (!BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported()) {
+            Toast.makeText(this, "Multiple advertisement not supported", Toast.LENGTH_SHORT).show();
+            btnScan.setEnabled(false);
+            btnStop.setEnabled(false);
+        }
+
+        getBluetoothAdapterAndLeScanner();
+
         if (btAdapter == null) {
             Toast.makeText(getApplicationContext(), "No Bluetooth detected", Toast.LENGTH_SHORT).show();
             finish();
         }
-
-        btnScan = (Button) findViewById(R.id.start_blue);
-        Button btnStop = (Button) findViewById(R.id.stop_blue);
 
         Switch switchTranScan = (Switch) findViewById(R.id.trans_receive);
         switchTranScan.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -94,14 +109,30 @@ public class BluetoothActivity extends AppCompatActivity {
             }
         });
 
-        TextView ap = (TextView) findViewById(R.id.blue_ap_choice);
-
         btnScan.setOnClickListener(v -> {
 
-            setAP = ap.getText().toString();
-            Log.i(TAG, setAP);
+            Log.i(TAG, btnScan.getText().toString());
 
-            scanLeDevice(scanning);
+            if (btnScan.getText().toString().equalsIgnoreCase("start scan")) {
+
+                discover();
+
+            } else if (btnScan.getText().toString().equalsIgnoreCase("start transmit")) {
+
+                transmit();
+            }
+        });
+
+        btnStop.setOnClickListener(v -> {
+
+            if (btnScan.getText().toString().equalsIgnoreCase("stop scan")) {
+
+                btAdapter.cancelDiscovery();
+
+            } else if (btnScan.getText().toString().equalsIgnoreCase("stop transmit")) {
+
+                stopTransmit();
+            }
         });
 
         listViewLE = (ListView) findViewById(R.id.listView1);
@@ -140,7 +171,7 @@ public class BluetoothActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        scanLeDevice(false);
+        btAdapter.cancelDiscovery();
     }
 
     @Override
@@ -171,7 +202,11 @@ public class BluetoothActivity extends AppCompatActivity {
 
             super.onScanResult(callbackType, result);
 
-            Log.i(TAG, result.getDevice().getName());
+            if (result == null || result.getDevice() == null) {
+                return;
+            }
+
+            Log.i(TAG, result.getDevice().getName() + "\n" + new String(result.getScanRecord().getServiceData(result.getScanRecord().getServiceUuids().get(0)), Charset.forName("UTF-8")));
 
             addBluetoothDevice(result.getDevice());
         }
@@ -180,10 +215,6 @@ public class BluetoothActivity extends AppCompatActivity {
         public void onBatchScanResults(List<ScanResult> results) {
 
             super.onBatchScanResults(results);
-            for (ScanResult result : results) {
-
-                addBluetoothDevice(result.getDevice());
-            }
         }
 
         @Override
@@ -203,36 +234,80 @@ public class BluetoothActivity extends AppCompatActivity {
         }
     };
 
-    private void scanLeDevice(final boolean enable) {
+    private void discover() {
 
-        if (enable) {
+        btnScan.setEnabled(false);
+        List<ScanFilter> filters = new ArrayList<>();
 
-            listBluetoothDevice.clear();
-            listViewLE.invalidateViews();
+        listBluetoothDevice.clear();
+        listViewLE.invalidateViews();
 
-            // Stops scanning after a pre-defined scan period.
-            handler.postDelayed(() -> {
+        ScanFilter filter = new ScanFilter.Builder()
+                .setServiceUuid(new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid))))
+                .build();
+        filters.add(filter);
 
-                bluetoothLeScanner.stopScan(scanCallback);
-                listViewLE.invalidateViews();
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .build();
 
-                Toast.makeText(this, "Scan timeout", Toast.LENGTH_LONG).show();
+        bluetoothLeScanner.startScan(filters, settings, scanCallback);
 
-                scanning = false;
-                btnScan.setEnabled(true);
-
-            }, SCAN_PERIOD);
-
-            bluetoothLeScanner.startScan(scanCallback);
-            scanning = true;
-            btnScan.setEnabled(false);
-
-        } else {
+        // Stops scanning after a pre-defined scan period.
+        handler.postDelayed(() -> {
 
             bluetoothLeScanner.stopScan(scanCallback);
+            listViewLE.invalidateViews();
+
+            Toast.makeText(this, "Scan timeout", Toast.LENGTH_LONG).show();
+
             scanning = false;
             btnScan.setEnabled(true);
-        }
+
+        }, SCAN_PERIOD);
+    }
+
+    private void transmit() {
+
+        btnScan.setEnabled(false);
+
+        advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
+                .setConnectable(false)
+                .build();
+
+        ParcelUuid pUuid = new ParcelUuid(UUID.fromString(getString(R.string.ble_uuid)));
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .addServiceUuid(pUuid)
+                .addServiceData(pUuid, "Data".getBytes(Charset.forName("UTF-8")))
+                .build();
+
+        advertisingCallback = new AdvertiseCallback() {
+
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertising onStartFailure: " + errorCode);
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        advertiser.startAdvertising(settings, data, advertisingCallback);
+    }
+
+    private void stopTransmit() {
+
+        btnScan.setEnabled(true);
+        advertiser.stopAdvertising(advertisingCallback);
     }
 
     /**
